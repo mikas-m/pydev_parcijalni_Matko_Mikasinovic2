840907d6a67a6193c24de22e36a507cb4ed75765
mikasm-sys <matko.mikas@gmail.com>
2024-12-22 13:35:50 +0100

    dodan zadnji zadatak bez provjere

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 import requests
+import datetime
+import json
 from sqlmodel import SQLModel, Field, create_engine, Session, Relationship, select
 from datetime import date
 from requests.models import HTTPError
@@ -15,29 +17,36 @@ class Task(SQLModel, table=True):
 def add_task(engine):
     while True:
         name = input("Unesi naziv zadatka: ")
-        date = input("Datum do kada zadatak mora biti ispunjen (u formatu YYYY-MM-DD): ")
+        date_for_task = random_date()
         random_priority = random.choice(["Nizak", "Srednji", "Visok"])
 
-        task = Task(name=name, date=date, priority=random_priority)
+        task = Task(name=name, date=date_for_task, priority=random_priority)
 
         if not add_to_db(engine, task, "Želiš dodati još jedan zadatak? da/ne: "):
             break
 
+def random_date():
+    now = datetime.datetime.now()
+    future_dates = [now + datetime.timedelta(days=i) for i in range(1, 36500)]
+    random_future_date = random.choice(future_dates)
+    date_for_task = random_future_date.strftime("%Y-%m-%d %H:%M:%S")
+    return date_for_task
 
-def show_tasks(engine):
+def show_sorted_tasks(engine):
     while True:
         user_choice_of_sort = input("Želiš li sortirati po [1] datumu dospijeća ili [2] prioritetu? ")
         if user_choice_of_sort == "1":
-            show_task(engine, Task.date)
+            show_sorted_task(engine, Task.date)
         elif user_choice_of_sort == "2":
-            show_task(engine, Task.priority)
+            show_sorted_task(engine, Task.priority)
         else:
-            if input("Želiš li probati opet? da/ne ") != "da":
-                break
+            print("Odabrao si broj koji se ne koristi.")
+        if input("Želiš li probati opet? da/ne ") != "da":
+            break
 
 
 def change_task(engine):
-    show_task(engine, Task.id)
+    show_sorted_task(engine, Task.id)
 
     id_task_to_change = input("ID zadatka kojega želiš promijeniti ")
 
@@ -57,7 +66,7 @@ def change_task(engine):
 
 
 def delete_task(engine):
-    show_task(engine, Task.id)
+    show_sorted_task(engine, Task.id)
 
     id_task_to_delete = input("ID zadatka kojega želiš izbrisati? ")
 
@@ -67,19 +76,31 @@ def delete_task(engine):
         session.delete(task_to_delete)
         session.commit()
 
-def synch():
-    URL = "https://jsonplaceholder.typicode.com/todos"
+
+def synch(engine):
+    URL = "https://jsonplaceholder.typicode.com/todos?_limit=200"
     try:
         response = requests.get(URL)
         response.raise_for_status()
 
-        data = response.json()
-        print(data["fact"])
+        datas = response.json()
+        print(len(datas))
+        for data in datas:
+            name = data["title"]
+            date_for_task = random_date()
+            random_priority = random.choice(["Nizak", "Srednji", "Visok"])
+            
+            task = Task(name=name, date=date_for_task, priority=random_priority)
+            with Session(engine) as session:
+                session.add(task)
+                session.commit()
+            
     except HTTPError as e:
         print(e)
 
 
 
+#POMOĆNE FUNKCIJE
 def add_to_db(engine, to_do, message):
     with Session(engine) as session:
         session.add(to_do)
@@ -89,15 +110,12 @@ def add_to_db(engine, to_do, message):
             False
         True
 
-def show_task(engine, db_table):
+def show_sorted_task(engine, db_table):
     with Session(engine) as session:
         select_all = select(Task).order_by(db_table)
         tasks = session.exec(select_all).all()
         for task in tasks:
-            print(f"ID -> {task.id}, Task to do -> {task.name}, Date to do it -> {task.date}, Priority -> {task.priority}")
-
-
-
+            print(f"ID -> {task.id}, Zadatak za napraviti -> {task.name}, Datum za napraviti -> {task.date}, Prioritet -> {task.priority}")
 
 
 def main():
@@ -117,13 +135,13 @@ def main():
         if choice == "1":
             add_task(engine)
         elif choice == "2":
-            show_tasks(engine)
+            show_sorted_tasks(engine)
         elif choice == "3":
             change_task(engine)
         elif choice == "4":
             delete_task(engine)
         elif choice == "5":
-            sync(engine)
+            synch(engine)
         elif choice == "6":
             break
         else:
